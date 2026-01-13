a31f81f200021fd2a6b34dab46f3d7459ceada98
Justin <justinestradacruz2006@gmail.com>
2026-01-13 14:16:39 -0600

    PracticaGuiada1

---
+++ PracticaGuiada1/src/Main.java
@@ -0,0 +1,17 @@
+public class Main{
+    public static void main(String[] args) {
+        // int[] numbers = {10,20,30,40,50};
+
+        // String[] fruits = new String[3];
+
+        // System.out.println(numbers[1] + fruits[1]);
+
+        Person p = new Person("Juan", 10, 2);
+
+        // System.out.println(p.getPets());
+
+        // for(Pet pet : p.getPets()){
+        //     System.out.println(pet.getPetName());
+        // }
+    }
+}
